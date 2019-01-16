4dee1c22febea2d921568a3ed6dcb59c0554d15b
siz0001 <siz0001@naver.com>
2019-01-16 15:06:21 +0900

    to do 완성

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import '@babel/polyfill';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+
+// 앱의 rest에서 store가 사용가능하도록 해주는 프로바이더(Provider)를 제공
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+// TodoApp을 import
+import TodoApp from "./TodoApp";
+
+// 기존 : ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  rootElement
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
